211fcf4267a6ef57db10017f9a3d88fb85ae1655
Sushil Deshmukh <sushil.deshmukh@joshsoftware.digital>
2022-12-27 15:54:49 +0530

    header and home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FoodDonationComponent } from './food-donation/food-donation.component';
+import { HomeScreenComponent } from './shared/components/home-screen/home-screen.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
   },
   {
     path:'home',
-    component:FoodDonationComponent,
+    component:HomeScreenComponent,
   }
 ];
 
